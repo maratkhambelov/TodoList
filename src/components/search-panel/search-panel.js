91880f8e2681cdbc58421934d5f0d65ab5f58b66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-03 10:04:13 +0000

    Deploy maratkhambelov/TodoList to github.com/maratkhambelov/TodoList.git:gh-pages

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './search-panel.css';
+
+export default class SearchPanel extends Component {
+
+
+    state = {
+        term: ''
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onSearchChange(term)
+
+    }
+
+    render () {
+        return (
+
+            <input className="form-control search-input"
+                   placeholder="Type here to search"
+                   onChange={this.onSearchChange}
+                   value={this.state.term}
+            />
+        );
+    }
+
+}
+
